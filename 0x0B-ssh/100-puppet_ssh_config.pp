1b6ce01173f9075f3bc2fa5e071c6117a7396b7a
Ian Kiguru <iankiguru72@outlook.com>
2023-09-24 12:21:30 +0300

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+# make changes to config file using Puppet
+
+include stdlib
+
+file {'etc/ssh/ssh_config': ensure => present,
+}
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
